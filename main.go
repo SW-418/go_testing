b28d1918c44185e2c2f66521ecdfb175ed3e77c6
SW-418 <samwells95@gmail.com>
2023-03-22 15:12:29 -0700

    Extract album model

--- main.go
+++ main.go
@@ -5,13 +5,6 @@ import (
     "github.com/gin-gonic/gin"
 )
 
-type album struct {
-    ID     string  `json:"id"`
-    Title  string  `json:"title"`
-    Artist string  `json:"artist"`
-    Price  float64 `json:"price"`
-}
-
 var albums = []album{
     {ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
     {ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
